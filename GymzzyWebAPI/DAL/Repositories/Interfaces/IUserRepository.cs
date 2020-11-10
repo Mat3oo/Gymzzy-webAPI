fbcc97d519e7d9220f80e70d8e9a245dc5df5d77
Mat3o <matlagit@outlook.com>
2020-11-10 10:10:09 +0100

    add UserRepository

---
+++ GymzzyWebAPI/DAL/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using GymzzyWebAPI.Models;
+
+namespace GymzzyWebAPI.DAL.Repositories.Interfaces
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+    }
+}
---
+++ GymzzyWebAPI/DAL/Repositories/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using GymzzyWebAPI.DAL.Repositories.Interfaces;
+using GymzzyWebAPI.Models;
+
+namespace GymzzyWebAPI.DAL.Repositories
+{
+    public class UserRepository : GenericRepository<User, UserContext>, IUserRepository
+    {
+        public UserRepository(UserContext context) : base(context)
+        {
+        }
+    }
+}
