d9c5de7a5287e8aac054beb5b6f8cd4102fb9d5a
Mat3o <matlagit@outlook.com>
2020-11-10 09:52:43 +0100

    add GenericRepository

---
+++ GymzzyWebAPI/DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using GymzzyWebAPI.DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymzzyWebAPI.DAL.Repositories
+{
+    public abstract class GenericRepository<TEntity, TContext> : IGenericRepository<TEntity>
+        where TEntity : class
+        where TContext : DbContext
+    {
+        private readonly TContext _context;
+
+        public GenericRepository(TContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+        }
+
+        public async Task<TEntity> GetAsync(Guid id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return _context.Set<TEntity>();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+        }
+    }
+}
---
+++ GymzzyWebAPI/DAL/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymzzyWebAPI.DAL.Repositories.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetAsync(Guid id);
+        IQueryable<TEntity> GetAll();
+        Task AddAsync(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
