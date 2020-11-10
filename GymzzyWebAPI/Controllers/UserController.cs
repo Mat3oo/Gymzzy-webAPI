d611dbe3128a3d4543a29d452b2085e56a830352
Mat3o <matlagit@outlook.com>
2020-11-10 11:12:28 +0100

    add UserController

---
+++ GymzzyWebAPI/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using GymzzyWebAPI.Models;
+using GymzzyWebAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace GymzzyWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<User> GetUser([FromBody] Guid id)
+        {
+            return await _userService.GetUserDetailsAsync(id);
+        }
+    }
+}
