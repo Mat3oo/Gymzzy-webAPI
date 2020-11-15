64a8e89c35ae49fca04b4c96c836d863c5e03208
Mat3o <matlagit@outlook.com>
2020-11-15 14:57:46 +0100

    add JwtTokenGeneratorService

---
+++ GymzzyWebAPI/Services/Interfaces/ITokenGeneratorService.cs
@@ -0,0 +1,9 @@
+﻿using GymzzyWebAPI.Models;
+
+namespace GymzzyWebAPI.Services.Interfaces
+{
+    public interface ITokenGeneratorService
+    {
+        public string GenerateToken(User user);
+    }
+}
---
+++ GymzzyWebAPI/Services/JwtTokenGeneratorService.cs
@@ -0,0 +1,41 @@
+﻿using GymzzyWebAPI.Models;
+using GymzzyWebAPI.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace GymzzyWebAPI.Services
+{
+    public class JwtTokenGeneratorService : ITokenGeneratorService
+    {
+        private readonly IConfigurationSection _jwtTokenSection;
+
+        public JwtTokenGeneratorService(IConfiguration configuration)
+        {
+            _jwtTokenSection = configuration.GetSection("JwtTokenOptions");
+        }
+
+        public string GenerateToken(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                            _jwtTokenSection.GetValue<string>("IssuerSigningKey")));
+
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            var claims = new[] {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            };
+
+            var token = new JwtSecurityToken(issuer: _jwtTokenSection.GetValue<string>("ValidIssuer"),
+              audience: _jwtTokenSection.GetValue<string>("ValidAudience"),
+              claims: claims,
+              notBefore: System.DateTime.Now,
+              expires: System.DateTime.Now.AddSeconds(_jwtTokenSection.GetValue<double>("ExpiresInSeconds")),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
