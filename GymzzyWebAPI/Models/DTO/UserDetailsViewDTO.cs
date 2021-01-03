e3fc98caa4bed8651a5e8902fb5d8f5183d87f86
Mat3o <matlagit@outlook.com>
2021-01-03 12:25:00 +0100

    no point to expose user Id by user details

--- GymzzyWebAPI/Models/DTO/UserDetailsViewDTO.cs
+++ GymzzyWebAPI/Models/DTO/UserDetailsViewDTO.cs
@@ -5,23 +5,29 @@ namespace GymzzyWebAPI.Models.DTO
 {
     public class UserDetailsViewDTO
     {
-        public Guid Id { get; set; }
         [StringLength(256)]
         public string Name { get; set; }
+
         [StringLength(256)]
         public string LastName { get; set; }
+
         [Required]
         [StringLength(256)]
         public string UserName { get; set; }
+
         [EmailAddress]
         public string Email { get; set; }
+
         [StringLength(maximumLength: 1, MinimumLength = 1)]
         [RegularExpression("[fm]")]
         public string Gender { get; set; }
+
         [Range(0, 300)]
         public float? Height { get; set; }
+
         [Range(0, 1000)]
         public float? Weight { get; set; }
+
         public DateTime? Birthday { get; set; }
     }
 }
